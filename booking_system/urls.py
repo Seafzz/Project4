5117812bd117870506a78c0e087547ee5d3c00bc
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-10-28 10:05:02 +0000

    updated url patterns in urls.py

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, booking_view, about
 
 urlpatterns = [
     path('', home, name='home'),
+    path('booking/', booking_view, name='booking'),
+    path('about/', about, name='about'),
 ]
