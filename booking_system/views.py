455ab3a37d6768b3fb69d5424bbce333e320a35f
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-10-28 09:26:37 +0000

    fix: add home view to render homepage template

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home (request):
-    return HttpResponse("WElcome to the booking System!")
+def home(request):
+    return render(request, 'home.html')
