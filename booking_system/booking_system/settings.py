ee93f2d699edad4a3eb1dda6bd27eead278a3476
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-10-28 06:38:55 +0000

    add my SQL url and installing dj-database-url

--- booking_system/booking_system/settings.py
+++ booking_system/booking_system/settings.py
@@ -74,10 +74,9 @@ WSGI_APPLICATION = "booking_system.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='postgres://u2f3ba2ipad:AQkEs0kWcKqo@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/vocal_step_hunt_879004'
+    )
 }
 
 
