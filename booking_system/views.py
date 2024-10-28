18bf41ab398849e597d8e6920d25356713113fb2
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-10-28 10:15:23 +0000

    updated views.py to include booking and about urls

--- booking_system/views.py
+++ booking_system/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def booking_view(request):
+    return render(request, 'booking.html')
+
+def about(request):
+    return render(request, 'about.html')
