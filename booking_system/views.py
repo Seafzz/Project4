d2e9976c4528b4ede2f3c681bc31d8c5bde1991b
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-15 16:29:12 +0000

    Update: updated the views to match the new path of the html sites

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
-
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'devconnect/home.html')
 
 def booking_view(request):
-    return render(request, 'booking.html')
+    return render(request, 'devconnect/booking.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'devconnect/about.html')
+
+def about (request):
+    return render (request, 'devconnect/booking_sucess.html')
