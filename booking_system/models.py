36d9a1126e235badd376bb2cbdc3a734cfa885c2
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-19 23:59:56 +0000

    add booking functionality with form handling and sucess page redirection

--- booking_system/models.py
+++ booking_system/models.py
@@ -6,3 +6,6 @@ class booking(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.EmailField()
     appointment_date = models.DateField()
+
+    def __stre__(self):
+        return f"{self.full_name} - {self.appointment_date}"
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.forms import Bookingform
 
 def home(request):
     return render(request, 'devconnect/home.html')
@@ -8,4 +9,16 @@ def booking_view(request):
 
 def about(request):
     return render(request, 'devconnect/about.html')
+
+
+def booking_view(request):
+    if request.method == "POST":
+        form = bookingform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('booking_success')
+    else:
+        form = bookingform()
+    
+    return render (request, 'devconnect/booking.html', {'form': form})
 
